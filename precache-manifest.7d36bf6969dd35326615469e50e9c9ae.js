f12e1d0185d9d44673af21cfa11cefc728f6b454
makniahmed-31 <makni.ahmed@gmail.com>
2020-09-30 15:51:33 +0100

    Updates

---
+++ precache-manifest.7d36bf6969dd35326615469e50e9c9ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad45e4f7dc7175f5144ad3db3620a534",
+    "url": "/React-State-Checkpoint/index.html"
+  },
+  {
+    "revision": "1c6795764e27ec00dc6a",
+    "url": "/React-State-Checkpoint/static/css/2.311aaba0.chunk.css"
+  },
+  {
+    "revision": "51c8b7a1925fb5148d6a",
+    "url": "/React-State-Checkpoint/static/css/main.8ccb0387.chunk.css"
+  },
+  {
+    "revision": "1c6795764e27ec00dc6a",
+    "url": "/React-State-Checkpoint/static/js/2.2bdd310b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-State-Checkpoint/static/js/2.2bdd310b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51c8b7a1925fb5148d6a",
+    "url": "/React-State-Checkpoint/static/js/main.7510adb2.chunk.js"
+  },
+  {
+    "revision": "335e5f6d29804b1e0949",
+    "url": "/React-State-Checkpoint/static/js/runtime-main.0dd0268b.js"
+  }
+]);
